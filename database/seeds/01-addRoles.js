62eff1f62b3e59760e96d88eb11cbc3bb41aa8f6
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-11-15 15:01:02 +0100

    better seeds

--- database/seeds/01-addRoles.js
+++ database/seeds/01-addRoles.js
@@ -1,11 +1,11 @@
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   return knex('roles').del()
     .then(function () {
       return knex('roles').insert([
-        { name: 'student'},
-        { name: 'instructor'},
-        { name: 'teamLead'}
+        { name: 'student' },
+        { name: 'instructor' },
+        { name: 'teamLead' }
       ]);
     });
 };
--- database/seeds/02-addUsers.js
+++ database/seeds/02-addUsers.js
@@ -3,9 +3,9 @@ exports.seed = function (knex) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
-        { email: 'student', password: '1234' },
-        { email: 'instructor', password: '1234' },
-        { email: 'teamLead', password: '1234' }
+        { email: 'peter@peter.com', password: '1234' },
+        { email: 'jean@jean.com', password: '1234' },
+        { email: 'dan@dan.com', password: '1234' }
       ]);
     });
 };
