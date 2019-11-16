c93ea3cf27c946c0db894af787cdbf605c6b9404
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-11-16 12:00:38 +0100

    comment

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
   env,
   port,
   pgdburl: process.env.DATABASE_URL, // postgres (see Luis video)
-  // for cookies to work cross origin we can't enable CORS for '*'
+  // for cookies to work with cross origin AJAX we can't enable CORS for '*'
   // the front end dev might ask to add other ursl to the admissible origins
   origin: ['https://cookies-liart-five.now.sh', 'http://localhost:3000'],
   secureCookie: isProduction
