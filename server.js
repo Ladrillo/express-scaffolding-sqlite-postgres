c51622f24eb649e6f2e36b3ae7696b5b40dc31b8
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-11-16 09:46:53 +0100

    set some cookies, add readme

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ app.use(cors())
 app.use(helmet())
 
 app.get('/api', (req, res) => {
+  res.cookie('lastEndpoint', '/')
   res.json({ server: 'up' })
 })
 
 app.get('/api/users', async (req, res) => {
+  res.cookie('lastEndpoint', '/users')
   const users = await db('users')
   res.json(users)
 })
 
 app.get('/api/roles', async (req, res) => {
+  res.cookie('lastEndpoint', '/roles')
   const roles = await db('roles')
   res.json(roles)
 })
