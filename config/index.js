3ffd1d336de47a34fdaa1840943f2e735fe12798
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-11-16 11:46:56 +0100

    clean up

--- config/index.js
+++ config/index.js
@@ -6,11 +6,10 @@ module.exports = {
   env,
   port,
   pgdburl: process.env.DATABASE_URL, // postgres (see Luis video)
-  // origin: isProduction // for cookies to work cross origin we can't enable CORS for '*'
-  //   ? 'https://cookies-liart-five.now.sh'
-  //   : 'http://localhost:3000',
+  // for cookies to work cross origin we can't enable CORS for '*'
+  // the front end dev might ask to add other ursl to the admissible origins
   origin: ['https://cookies-liart-five.now.sh', 'http://localhost:3000'],
-  secureCookie: isProduction // over https only?
-    ? true
+  secureCookie: isProduction
+    ? true // cookie only sent over https
     : false,
 }
--- server.js
+++ server.js
@@ -13,28 +13,30 @@ var corsOptions = {
 
 const cookieConfig = {
   maxAge: 900000,
-  httpOnly: false, // should be true to defend from XSS
+  httpOnly: false, // should be true to defend against XSS
   sameSite: 'None',
   secure
+}
+
+const lastEndpointUsed = last => (_, res, next) => {
+  res.cookie('lastEndpoint', last, cookieConfig)
+  next()
 }
 
 app.use(express.json())
 app.use(cors(corsOptions))
 app.use(helmet())
 
-app.get('/api', (req, res) => {
-  res.cookie('lastEndpoint', 'api', cookieConfig)
+app.get('/api', lastEndpointUsed('api'), (_, res) => {
   res.json({ server: 'up' })
 })
 
-app.get('/api/users', async (req, res) => {
-  res.cookie('lastEndpoint', 'api-users', cookieConfig)
+app.get('/api/users', lastEndpointUsed('api-users'), async (_, res) => {
   const users = await db('users')
   res.json(users)
 })
 
-app.get('/api/roles', async (req, res) => {
-  res.cookie('lastEndpoint', 'api-roles', cookieConfig)
+app.get('/api/roles', lastEndpointUsed('api-roles'), async (_, res) => {
   const roles = await db('roles')
   res.json(roles)
 })
