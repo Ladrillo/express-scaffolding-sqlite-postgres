0cd558c0a9c7834f0d65918b6a024b133e809d1e
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-11-17 13:23:29 +0100

    put everything back

--- config/index.js
+++ config/index.js
@@ -6,9 +6,9 @@ module.exports = {
   env,
   port,
   pgdburl: process.env.DATABASE_URL, // postgres (see Luis video)
-  // for cookies to work with cross origin AJAX we can't enable CORS for '*'
-  // the front end dev might ask to add other ursl to the admissible origins
-  origin: ['https://cookies-liart-five.now.sh', 'http://localhost:3000'],
+  // for cookies to work with SPA + API on different domain we can't enable CORS for '*'
+  // the front end dev might ask to add other URLs to the admissible origins
+  origin: ['https://cookies-liart-five.now.sh', 'http://127.0.0.1:3000', 'http://localhost:3000'],
   secure: isProduction, // cookie only sent over https
   domain: isProduction
     ? 'herokuapp.com'
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const session = require('express-session')
 const db = require('./database/dbConfig')
-const { secure, origin, domain } = require('./config/index')
+const { secure, origin } = require('./config/index')
 
 const app = express()
 
@@ -24,57 +24,38 @@ const sessionConfig = {
   saveUninitialized: false,
 };
 
-// app.use(helmet())
+const cookieConfig = {
+  maxAge: 1000 * 60 * 60,
+  httpOnly: false,
+  sameSite: 'None',
+  secure,
+}
+
+app.use(helmet())
 app.use(express.json())
 app.use(cors(corsOptions))
-// app.use(session(sessionConfig))
+app.use(session(sessionConfig))
 
 app.get('/api', (_, res) => {
-  res.cookie('lastEndpoint', 'api', {
-    maxAge: 1000 * 60 * 60,
-    httpOnly: false,
-    sameSite: 'None',
-    secure,
-    domain,
-    path: '/',
-  })
+  res.cookie('lastEndpoint', 'api', cookieConfig)
   res.json({ server: 'up' })
 })
 
-app.get('/api/login', (req, res) => {
-  res.cookie('lastEndpoint', 'api-login', {
-    maxAge: 1000 * 60 * 60,
-    httpOnly: false,
-    sameSite: 'None',
-    secure,
-    domain,
-  })
-  // req.session.user = { name: req.query.name }
-  res.json({ user: req.query.name })
-})
-
 app.get('/api/users', async (_, res) => {
-  res.cookie('lastEndpoint', 'api-users', {
-    maxAge: 1000 * 60 * 60,
-    httpOnly: false,
-    sameSite: 'None',
-    secure,
-    domain,
-  })
+  res.cookie('lastEndpoint', 'api-users', cookieConfig)
   const users = await db('users')
   res.json(users)
 })
 
 app.get('/api/roles', async (_, res) => {
-  res.cookie('lastEndpoint', 'api-roles', {
-    maxAge: 1000 * 60 * 60,
-    httpOnly: false,
-    sameSite: 'None',
-    secure,
-    domain,
-  })
+  res.cookie('lastEndpoint', 'api-roles', cookieConfig)
   const roles = await db('roles')
   res.json(roles)
+})
+
+app.get('/api/login', (req, res) => {
+  req.session.user = { name: req.query.name }
+  res.json({ user: req.query.name })
 })
 
 module.exports = app
