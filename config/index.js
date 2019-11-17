489c2e26bbfb7ae5a91217f83ae1df34a3303f9b
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-11-17 13:49:50 +0100

    wip

--- config/index.js
+++ config/index.js
@@ -7,7 +7,6 @@ module.exports = {
   port,
   pgdburl: process.env.DATABASE_URL, // postgres (see Luis video)
   // for cookies to work with SPA + API on different domain we can't enable CORS for '*'
-  // the front end dev might ask to add other URLs to the admissible origins
   origin: [
     'https://cookies-liart-five.now.sh',
     'https://cookies.ladrillo.now.sh/',
